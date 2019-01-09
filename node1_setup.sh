a60cd7b6e9be188d9b4473a05e1e9eedb629c591
Wilfred <wtcsmail@gmail.com>
2019-01-09 10:50:16 +0800

    bug fix

--- node1_setup.sh
+++ node1_setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Start the master and apply CNI
-kubeadm init --apiserver-advertise-address $(hostname -i)
+kubeadm init --apiserver-advertise-address $(hostname -i) 2>&1 |tee -a ~/kubeadm_init.log
 kubectl apply -n kube-system -f \
     "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 |tr -d '\n')"
 
+# Add some packages for easier usage
+yum install binutils vim net-tools openssl grep bash-completion -y
+
 # apply kubectl auto-completion
 source <(kubectl completion bash)
+#
+echo see ~/kubeadm_init.log for kubeadm logs
 
-# Add some packages for easier usage
-yum install binutils vim net-tools -y
+echo "To join in other nodes:"
+echo "kubeadm join $(kubectl cluster-info |grep master|egrep -o 'https://\S+') --token $(kubeadm token list|grep authentication|cut -d' ' -f1) --discovery-token-ca-cert-hash sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null |openssl dgst -sha256 -hex|sed 's/^.* //')"
+
